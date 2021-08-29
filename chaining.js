d0ba9740552eff99c3e85e2348aa81b2a684b6cf
itspeomd <peomd04@gmail.com>
2021-08-30 01:55:53 +0600

    chaining

---
+++ chaining.js
@@ -0,0 +1,20 @@
+
+const company = {
+
+    name: "gp",
+
+    ceo: {
+        id: 1,
+        names: "peom",
+        position: "data scientist",
+    },
+
+    web: {
+        work: "web-dev",
+        employee: 22,
+        framework: "bootstrap"
+    }
+
+};
+
+console.log(company?.ceo?.names);
