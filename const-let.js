4daa97ad720a50d9627e212e8e2d49be35f7b426
itspeomd <peomd04@gmail.com>
2021-08-29 22:40:18 +0600

    const-let

---
+++ const-let.js
@@ -0,0 +1,22 @@
+const firstName = "Peom";
+const lastName = "Dutta";
+
+let a = 5;
+let b = 10;
+
+const fullName = firstName + " " + lastName;
+
+a = 10;
+b = 5;
+
+const fullSum = a + b;
+
+console.log(fullName);
+
+console.log(fullSum);
+
+// string using backtik literal 
+
+const newFullNameNumber = `${firstName} ${lastName} ${a} ${b}`;
+
+console.log(newFullNameNumber);
